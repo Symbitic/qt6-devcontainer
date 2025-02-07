1005c4156e4072e30b26f365be48a2dc709b425a
Alex Shaw <alex.shaw.as@gmail.com>
2025-02-07 11:02:49 -0800

    fix: disable vscode auto-adding files.associations

--- .devcontainer/qt6/install.sh
+++ .devcontainer/qt6/install.sh
@@ -91,7 +91,7 @@ then
 fi
 
 # Install dependencies
-check_packages curl ca-certificates gnupg2 dirmngr unzip bash-completion build-essential cmake \
+check_packages curl ca-certificates gnupg2 dirmngr unzip build-essential cmake \
     libgl1-mesa-dev libgstreamer-gl1.0-0 libpulse-dev libxcb-glx0 libxcb-icccm4 libxcb-image0 \
     libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-render0 libxcb-shape0 libxcb-shm0 \
     libxcb-sync1 libxcb-util1 libxcb-xfixes0 libxcb-xinerama0 libxcb1 libxkbcommon-dev \
